15e2154e81e0f3f7156dd511ec3d4a2ee1742ec0
ssannejohansson <sannej0hansson@hotmail.com>
2025-03-19 11:17:00 +0100

    Finished assignment
    Finished assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+// Assignment 2 
+
+prompt("enter a price tag")
+
+
+let price = "$29.39";
+console.log(price);
+
+
+price = parseFloat("$29.39".slice(1));
+console.log(typeof price); 
+console.log(price);
+
+
+price = price * 0.9;
+console.log(price);
+
+console.log(`Your new price is $${price}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
